8bba7230bd3d213bdbb23f095aed87e3e4de79fb
ana301 <31302345+ana301@users.noreply.github.com>
2020-08-26 21:48:33 +0530

    Update GetPairFromColor.cs

--- TelCo.ColorCoder/GetPairFromColor.cs
+++ TelCo.ColorCoder/GetPairFromColor.cs
@@ -15,9 +15,9 @@ namespace TelCo.ColorCoder
         {
             // Find the major color in the array and get the index
             int majorIndex = -1;
-            for (int i = 0; i <colorMapMajor.Length; i++)
+            for (int i = 0; i <colorDataMember.colorMapMajor.Length; i++)
             {
-                if (colorMapMajor[i] == pair.majorColor)
+                if (colorDataMember.colorMapMajor[i] == pair.majorColor)
                 {
                     majorIndex = i;
                     break;
@@ -25,9 +25,9 @@ namespace TelCo.ColorCoder
             }
             // Find the minor color in the array and get the index
             int minorIndex = -1;
-            for (int i = 0; i < colorMapMinor.Length; i++)
+            for (int i = 0; i < colorDataMember.colorMapMinor.Length; i++)
             {
-                if (colorMapMinor[i] == pair.minorColor)
+                if (colorDataMember.colorMapMinor[i] == pair.minorColor)
                 {
                     minorIndex = i;
                     break;
@@ -41,7 +41,7 @@ namespace TelCo.ColorCoder
             }
             // Compute pair number and Return  
             // (Note: +1 in compute is because pair number is 1 based, not zero)
-            return (majorIndex * colorMapMinor.Length) + (minorIndex + 1);
+            return (majorIndex * colorDataMember.colorMapMinor.Length) + (minorIndex + 1);
         }
     }
 }
